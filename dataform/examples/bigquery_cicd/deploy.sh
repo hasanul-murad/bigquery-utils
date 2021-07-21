6396fefcdfa6bb6f84022475babce98da421cfcf
danieldeleo <ddeleo@google.com>
2021-07-21 10:01:39 -0400

    Add final step to deploy.sh which cleans up files that were created for building/deploying

--- dataform/examples/bigquery_cicd/deploy.sh
+++ dataform/examples/bigquery_cicd/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cleanup() {
+ rm -rf node_modules
+ rm -f package-lock.json
+ rm -f .df-credentials.json
+}
+
 dataform install
 echo '{"projectId": "", "location": "US"}' > .df-credentials.json
 dataform run
+
+# Cleaning only necessary when running locally
+cleanup
