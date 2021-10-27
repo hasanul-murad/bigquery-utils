f6ba2ad76882d8f431da0dfe024a2b7294cd8520
ddeleo <danieldeleo@google.com>
2021-10-27 09:28:45 -0400

    Adding dataform standard ci/cd example

--- dataform/examples/bigquery_cicd/run.sh
+++ dataform/examples/bigquery_cicd/run.sh
@@ -2,4 +2,13 @@
 
 echo "{\"projectId\": \"${PROJECT_ID}\", \"location\": \"${BQ_LOCATION}\"}" > .df-credentials.json
 dataform install
-dataform run --actions "${DATAFORM_ACTIONS}" --tags "${DATAFORM_TAGS}"
+
+if [[ -n "${DATAFORM_ACTIONS}" && -n "${DATAFORM_TAGS}" ]]; then
+  dataform run --actions "${DATAFORM_ACTIONS}" --tags "${DATAFORM_TAGS}"
+elif [[ -n "${DATAFORM_ACTIONS}" ]]; then
+  dataform run --actions "${DATAFORM_ACTIONS}"
+elif [[ -n "${DATAFORM_TAGS}" ]]; then
+  dataform run --tags "${DATAFORM_TAGS}"
+else
+  dataform run
+fi
