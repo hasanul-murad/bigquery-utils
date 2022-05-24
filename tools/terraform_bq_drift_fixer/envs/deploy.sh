0fc2832bfc21dbe6c09fa5173d8b1f2452b0464c
ddeleo <danieldeleo@google.com>
2022-05-24 13:38:24 -0400

    update

--- tools/terraform_bq_drift_fixer/envs/deploy.sh
+++ tools/terraform_bq_drift_fixer/envs/deploy.sh
@@ -3,13 +3,15 @@
 env=$1
 tool=$2
 
-terragrunt run-all apply -refresh-only --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
-terragrunt run-all show -json --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}" > state.json
-#terragrunt run-all plan -json --terragrunt-non-interactive --terragrunt-working-dir=qa > terraform_output.json
-terragrunt run-all plan --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
+#terragrunt run-all apply -refresh-only --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
+#terragrunt run-all show -json --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}" > state.json
+#terragrunt run-all plan -json --terragrunt-non-interactive --terragrunt-working-dir=qa > terraform_plan_out.json
+#terragrunt run-all plan --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
 #terragrunt run-all apply -refresh-only --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
 #terragrunt run-all apply --terragrunt-non-interactive --terragrunt-working-dir="${env}"/"${tool}"
 #terragrunt run-all destroy --terragrunt-non-interactive
+
+terragrunt run-all apply --terragrunt-non-interactive
 
 # Uncomment below to purge the terragrunt caches
  find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
